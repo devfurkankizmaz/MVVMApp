5a9b81b4b4af7b2f75f4db3a71b81664427e3cb1
devfurkankizmaz <dev.furkan@outlook.com>
2023-08-10 20:02:11 +0300

    Initial Commit

---
+++ MyMVVMApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMVVMApp
+//
+//  Created by Furkan Kızmaz on 10.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
